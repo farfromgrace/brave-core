940a37b43785c200ea425a636de6c314ec8c23e8
samartnik <artem@brave.com>
2022-02-04 17:23:44 -0500

    [Android] Fixes for context menu test

--- android/javatests/org/chromium/chrome/browser/contextmenu/BraveContextMenuPopulatorTest.java
+++ android/javatests/org/chromium/chrome/browser/contextmenu/BraveContextMenuPopulatorTest.java
@@ -9,14 +9,30 @@ import static org.junit.Assert.assertFalse;
 
 import androidx.test.filters.SmallTest;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
+import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+
+import java.util.concurrent.ExecutionException;
 
 @RunWith(BaseJUnit4ClassRunner.class)
+@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class BraveContextMenuPopulatorTest {
+    @Rule
+    public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
+
+    @Before
+    public void setUp() throws ExecutionException {
+        mActivityTestRule.startMainActivityFromLauncher();
+    }
+
     @Test
     @SmallTest
     public void testTabGroupAutoCreationPreference() {
